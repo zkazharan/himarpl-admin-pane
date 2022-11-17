f4251c9ca87f602933b9a610dce57deffce35917
Azka A. Azharan <azka.azharan@gmail.com>
2022-11-18 01:29:24 +0700

    optimize sidebar and navbar

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Sidebar from 'components/Sidebar/Sidebar.js';
 import HeaderStats from 'components/Headers/HeaderStats.js';
 import FooterAdmin from 'components/Footers/FooterAdmin.js';
+import Navbar from 'components/Navbars/IndexNavbar.js'
 
 // views
 import Dashboard from 'views/admin/Dashboard.js';
@@ -14,16 +15,18 @@ import Tables from 'views/admin/Tables.js';
 export default function Admin() {
   return (
     <>
-      <Sidebar />
-      <div className='relative md:ml-64 bg-gray-800 h-screen pt-24'>
-        <div className='px-4 md:px-10 mx-auto w-full  -m-24 bg-gray-800 pt-12'>
-          <Routes>
-            <Route path='/dashboard' exact element={<Dashboard />} />
-            <Route path='/settings' exact element={<Settings />} />
-            <Route path='/tables' exact element={<Tables />} />
-          </Routes>
-          <FooterAdmin />
-          
+      <Navbar />
+      <div className=''>
+        <Sidebar />
+        <div className='relative md:ml-64 md:mt-20 bg-gray-800 h-screen pt-24'>
+          <div className='px-4 md:px-10 mx-auto w-full  -m-24 bg-gray-800 pt-12'>
+            <Routes>
+              <Route path='/dashboard' exact element={<Dashboard />} />
+              <Route path='/settings' exact element={<Settings />} />
+              <Route path='/tables' exact element={<Tables />} />
+            </Routes>
+            <FooterAdmin />
+          </div>
         </div>
       </div>
     </>
